d6872329ca9673c06597cad2ba1aaedc993f22ea
Duane <duanerichard47@hotmail.com>
2023-01-08 19:48:27 -0500

    Completed lab Phase-1 Logical Operators

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  
+  if (distance <= 400){
+    return 'This one is on me!';
+  }
+  else if (distance >= 400 && distance<= 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (distance > 2500){
+       return 'No can do.';
+  }
+  
+  }
   // Write your code here!
+
+
+function ternaryCheckCity(whichCity){
+  // Write your code here!
+  return (whichCity === 'NYC' ? "Ok, sounds good." : "No go."); 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tipAmount){
+  
   // Write your code here!
+  switch(tipAmount){
+      case "generous":
+      return "Thank you so much.";
+      break;
+      case "not as generous" :
+      return "Thank you."
+      break;
+      default:
+      return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
+
+  
